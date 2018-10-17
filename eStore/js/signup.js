25dd7ba52d14f65fa2c375c674ea7371d7254095
SushmaKrishna <sushmasakala@unomaha.edu>
2018-10-17 16:19:37 -0500

    Signup updated
    Error scenarios included

--- eStore/js/signup.js
+++ eStore/js/signup.js
@@ -1,27 +1,61 @@
 $(document).ready(function () {
 
-    $('#signUpForm').validate({
-        rules: {
-            userId: {
-                minlength: 3,
-                required: true
-            },
-            pwd: {
-                required: true,
-                minlength: 2
-            },
-            confPwd: {
-                minlength: 2,
-                required: true
-            }
-        },
-        highlight: function (element) {
-            $(element).closest('.form-group').removeClass('success').addClass('error');
-        },
-        success: function (element) {
-            element.addClass('valid')
-                .closest('.form-group').removeClass('error').addClass('success');
+    $("#signUpForm").submit(function (e) {
+        e.preventDefault();
+        var signUpData={};   
+        var validFlag=true;  
+        var pwdFlag=true;
+        $.each($(this).serializeArray(), function (i, field) {
+            signUpData[field.name] = field.value;
+        });
+
+        validFlag=validateSignUp();
+        pwdFlag=validatePassword(signUpData);
+
+        if(validFlag && pwdFlag){
+            saveSignUpData(signUpData);
         }
+
     });
+
+    function validateSignUp(){
+        var validFlag=true
+        $(".form-group").each(function () {
+            if ($(this).find("input").length>0){
+            
+            if(!($(this).find("input").get(0).value)) {
+                $(this).addClass("has-error");
+                validFlag=false;
+            }else{
+                $(this).removeClass("has-error");  
+            }
+        }
+        if ($(this).find("textarea").length>0){            
+            if(!($(this).find("textarea").get(0).value)) {
+                $(this).addClass("has-error"); 
+                validFlag=false;         
+            }else{
+                $(this).removeClass("has-error");  
+            }
+        }
+        });
+        return validFlag;
+    }
+
+    function validatePassword(signUpData){
+        var validFlag=true
+        if (signUpData["pwd"] != signUpData["confPwd"]) {
+            $("#confPwd").closest( ".form-group" ).addClass("has-error");
+            validFlag=false;
+        }else{
+            if(signUpData["confPwd"]){
+                $("#confPwd").closest( ".form-group" ).removeClass("has-error");
+            }
+       }
+       return validFlag;
+    }
+    function saveSignUpData(signUpData){
+        console.log(signUpData)
+    }
 
 });
